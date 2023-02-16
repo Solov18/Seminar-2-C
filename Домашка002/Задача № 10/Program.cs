e02337608a52aae01e9ab3a53b507ac85655071b
Solov18 <solov18@list.ru>
2023-02-16 10:47:33 +0300

    10

--- Домашка002/Задача № 10/Program.cs
+++ Домашка002/Задача № 10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int num = new Random().Next(100,1000);
+
+int centr = num / 10 % 10;
+
+string ansStr = centr.ToString();
+Console.Write($"{num} -> {ansStr}");
+
+
+
+
