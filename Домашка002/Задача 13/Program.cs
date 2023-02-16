6ba01873bccea61818a248ca44c2fcedecca137d
Solov18 <solov18@list.ru>
2023-02-16 22:43:27 +0300

    no  13

--- Домашка002/Задача 13/Program.cs
+++ Домашка002/Задача 13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+int num = new Random().Next();
+if (num > 999)
+{
+    string ansStr = (num /= 10).ToString();
+    string ansStr2 = (num % 10).ToString(); 
+    Console.Write($"{num} -> {ansStr2}");
+}
+else if (num < 100)
+{
+    Console.Write($"{num} -> Третей цыфры нет ");
+}
